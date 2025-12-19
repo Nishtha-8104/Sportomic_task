f451d12ea2ec53eabead3f57ae0b69bb923e1596
Nishtha-8104 <143199132+Nishtha-8104@users.noreply.github.com>
2025-12-19 15:11:30 +0530

    Change database connection string in db.js
    Updated the connection string to use a specific PostgreSQL database.

--- server/config/db.js
+++ server/config/db.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 //   host: "localhost",
 //   port: 5432
 
-    connectionString: process.env.DATABASE_URL + "?sslmode=require",
+    connectionString: "postgresql://task1_dcz3_user:dboOBio31s8eysCBYT4MONJoPAbLH4KD@dpg-d52fibffte5s73d4u1c0-a.singapore-postgres.render.com/task1_dcz3" + "?sslmode=require",
     // ssl: {
     //   rejectUnauthorized: require  // This allows the connection without certificate validation
     // }
